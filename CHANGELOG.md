334cd0615927a634733c97b498057879c4543cea
Greg Allen <me@jga.me>
2012-03-04 16:08:11 -0800

    updated changelog and re-built

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+# v0.0.3 (03/04/2012)
+- aug(true, {}, {}) to do deep augment
 # v0.0.2
 - made augmenting functions more generic. (don't assume aug prototype)
 # v0.0.1
--- dist/aug.js
+++ dist/aug.js
@@ -8,11 +8,19 @@
 
 var aug = function __aug() {
   var args = Array.prototype.slice.call(arguments);
+  var deep = false;
   var org = args.shift();
+  if (typeof org === 'boolean') {
+    deep = true;
+    org = args.shift();
+  }
   for (var i = 0, c = args.length; i < c; i++) {
     var prop = args[i];
     for (var name in prop) {
-      org[name] = prop[name];
+      if (deep && typeof prop[name] === 'object')
+        aug(org[name], prop[name]);
+      else
+        org[name] = prop[name];
     }
   }
   return org;
