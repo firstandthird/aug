7b5979017711f58d47a2256b8870936ce5382af8
Greg Allen <me@jga.me>
2012-11-05 17:57:34 -0800

    grunt

--- dist/aug.js
+++ dist/aug.js
@@ -1,10 +1,10 @@
 /*!
-  * aug.js - A javascript library to extend existing objects and prototypes 
-  * v0.0.3
-  * https://github.com/jgallen23/aug
-  * copyright JGA 2011
-  * MIT License
-  */
+ * aug - a library to augment objects and prototypes
+ * v0.0.4
+ * https://github.com/jgallen23/aug
+ * copyright JGA 2012
+ * MIT License
+*/
 
 var aug = function __aug() {
   var args = Array.prototype.slice.call(arguments);
